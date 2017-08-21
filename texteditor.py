ded9d8418a7a845845368ed02000e60ab8c829c4
Lucas Lee <lucasleeeeeeeee@gmail.com>
2017-08-21 19:06:26 +0800

    clean up some code

--- texteditor.py
+++ texteditor.py
@@ -1,36 +1,51 @@
 from tkinter import *
 import tkinter, tkinter.filedialog
-root=Tk("PyEdit");
-text=Text(root);
-text.grid();
-hasopened = False;
-def fileopen():
-    global hasopened
-    global file
-    hasopened = True;
-    file = tkinter.filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")));
-    f = open(file,'r+');
-    dlg = f.read();
-    text.insert(INSERT, dlg);
-    f.close();
-    return file
-def saveas():
-    try:
+class m:
+    def __init__(self, file, f, t, hasopened, dlg, save):
+        self.file = file;
+        self.f = f;
+        self.t = t;
+        self.hasopened = hasopened;
+        self.dlg = dlg;
+        self.save = save;
+        self.hasopened = False;
+    def fileopen():
+        hasopened = True;
+        file = tkinter.filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")));
+        f = open(file,'r+');
+        dlg = f.read();
+        text.insert(INSERT, dlg);
+        f.close();
+        button2=Button(root, text="Save", command=m.saveas);
+        button2.grid(row=10, column=2);
+        return file
+    def saveas():
+        save = tkinter.filedialog.asksaveasfilename();
         t = text.get("1.0", "end-1c");
-        f = open(save, "w+");
+        f = open(save,'w');
         f.write(t);
         f.close();
-    except:
+    def new():
         save = tkinter.filedialog.asksaveasfilename();
         t = text.get("1.0", "end-1c");
         f = open(file,'w');
         f.write(t);
         f.close();
-button=Button(root, text="Save", command=saveas);
+    def save():
+       t = text.get("1.0", "end-1c");
+       f = open(self.file,'w');
+       f.write(t);
+       f.close();
+
+
+root=Tk("PyEdit");
+text=Text(root);
+text.grid();
+hasopened = False;
+button=Button(root, text="Save as", command=m.saveas);
 button.grid(row=10, column=0);
-button1=Button(root, text="Open", command=fileopen);
+button1=Button(root, text="Open", command=m.fileopen);
 button1.grid(row=10, column=1);
-if (hasopened == True):
-    button2=Button(root, text="Save", command=save);
-    button2.grid(row=10, column=2);
+button2=Button(root, text="Save", command=m.save, state=DISABLED);
+button2.grid(row=10, column=2);
 root.mainloop();
